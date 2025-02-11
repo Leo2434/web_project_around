51bab8fd0b82fdab66e2da4f280985ce0c03c6be
Leosjt <sebas.jurado34@gmail.com>
2025-02-11 02:38:57 -0500

    Refactorizar la carga de los cards iniciales

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,8 @@ const openProfileButton = document.querySelector("#open-profile-button");
 // const openCardButton = document.querySelector("#open-card-button");
 const closeProfileButton = document.querySelector("#close-profile-button");
 // const closeCardButton = document.querySelector("#close-card-button");
+const cardTemplate = document.querySelector(".template");
+const cardsContent = document.querySelector(".cards__content");
 
 const initialCards = [
   {
@@ -34,9 +36,38 @@ const initialCards = [
   },
 ];
 
+function loadInitialCards(initialCards) {
+  initialCards.forEach(function (item) {
+    createCard(item.name, item.link);
+  });
+}
+
+function createCard(name, link) {
+  const cardElement = cardTemplate
+    .cloneNode(true)
+    .content.querySelector(".card");
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement
+    .querySelector(".card__like-button")
+    .addEventListener("click", function (evt) {
+      evt.target.classList.toggle("card__black-like-button");
+    });
+  // evt.target.style.backgroundImage = "url('../images/cards/black-like.svg')";
+  // Esta forma funciona pero no permite volver a poner el corazon blanco
+
+  cardImage.addEventListener("click", function () {
+    handleOpenPopup(profilePopup); //cambiar para aparecer el popup de la imagen
+  });
+  cardsContent.prepend(cardElement);
+}
+
 function handleOpenPopup(popupElement) {
   popupElement.classList.add("popup_opened");
 }
+
 function handleClosePopup(popupElement) {
   popupElement.classList.remove("popup_opened");
 }
@@ -73,4 +104,5 @@ function handleProfileFormSubmit(evt) {
   handleClosePopup(profilePopup);
 }
 
+loadInitialCards(initialCards);
 profileForm.addEventListener("submit", handleProfileFormSubmit);
